7f53294907d310ce1245753b0acba69eddeb1050
Erik Rosvall <erirosv@gmail.com>
2023-06-09 17:19:22 +0200

    the code can read the datasets, the convertion code is not working, it needs to be rewritten or something

--- dependencies/data_code/convert_data_for_training.py
+++ dependencies/data_code/convert_data_for_training.py
@@ -15,6 +15,21 @@ csv_files = glob.glob(os.path.join(path, '*.csv'))
 # - Run it through: prepare_dataset_for_modeling
 # - stor the converted datafile and run through all the datasets
 
+def convert_csv(files):
+    lst = []
+    for f in files:
+        print(f)
+        x, y = prepare_dataset_for_modeling(f, pred_type='c')
+        df = pd.DataFrame({'data':x, 'label':y})
+        print(df)
+        lst.append(df)
+    return lst
+
+#print(csv_files)
+cc = convert_csv(csv_files)
+print(cc)
+
+'''
 for f in csv_files:
       
     # read the csv file
@@ -28,3 +43,4 @@ for f in csv_files:
     print(f'Content: {df}')
     #display(df)
     print()
+'''
