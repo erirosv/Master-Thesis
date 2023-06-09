29f09d429bf1302958969fd6a5411fb1fc788166
Erik Rosvall <erirosv@gmail.com>
2023-06-09 18:38:50 +0200

    error is in the concat. they add the file name to the complete pateh somewhere in the code

--- dependencies/data_code/convert_data_for_training.py
+++ dependencies/data_code/convert_data_for_training.py
@@ -6,28 +6,32 @@ from prepare_dataset_for_modeling import prepare_dataset_for_modeling
 
 PATH = '../../data/processed'
 
-#path = os.getcwd(PATH)
 path = os.path.abspath(PATH)
 csv_files = glob.glob(os.path.join(path, '*.csv'))
+print(f'PATH: {path}')
+print(f'CSV_FILES: {csv_files}')
 
-# TODO:
-# - grab the first csv file 
-# - Run it through: prepare_dataset_for_modeling
-# - stor the converted datafile and run through all the datasets
+lst = []
 
 def convert_csv(files):
-    lst = []
     for f in files:
-        print(f)
-        x, y = prepare_dataset_for_modeling(f, pred_type='c')
-        df = pd.DataFrame({'data':x, 'label':y})
-        print(df)
+        dataset_name = os.path.basename(f)  # Extract the dataset name from the file path
+        data_directory = os.path.dirname(f) 
+
+        print(f'Processing dataset: {dataset_name}')
+        print(f'directory: {data_directory}')
+
+        file_path = os.path.join(data_directory, dataset_name)  # Concatenate directory and dataset name correctly
+        print(f'FILE_PATH: {file_path}')
+
+        x, y = prepare_dataset_for_modeling(dataset_name, pred_type='c', data_directory=file_path)
+        df = pd.DataFrame({'data': x, 'label': y})
         lst.append(df)
     return lst
 
-#print(csv_files)
 cc = convert_csv(csv_files)
 print(cc)
+
 
 '''
 for f in csv_files:
