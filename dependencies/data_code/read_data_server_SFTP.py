de85c18ff6d0ca49bbc200daa344f89c26b55d4a
Erik Rosvall <erirosv@gmail.com>
2023-06-09 18:07:19 +0200

    trying to get remote/local-path to work

--- dependencies/data_code/read_data_server_SFTP.py
+++ dependencies/data_code/read_data_server_SFTP.py
@@ -7,8 +7,10 @@ load_dotenv()
 server = os.getenv('SERVER')
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
-file_path = os.getenv('FILE_PATH')
+file_path = os.getenv('FILE_PATH_2')
 port = os.getenv('PORT')
+
+local_filepath = '/home/rusty/fun/Master-Thesis/dependencies/data_code/data'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -18,7 +20,7 @@ client.connect(server, port=port, username=username, password=password)
 sftp = client.open_sftp()
 
 # Läs in filen
-sftp.get(remote_filepath, local_filepath)
+sftp.get(file_path, local_filepath)
 
 # Stäng SFTP-sessionen och SSH-anslutningen
 sftp.close()
